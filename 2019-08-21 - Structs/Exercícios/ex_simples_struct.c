0229b48e27d05926e1076226d700b9b1b76ca338
emanoelim <emanoeli.madalosso@gmail.com>
2019-08-22 09:01:06 -0300

    exercico corrigido

--- 2019-08-21 - Structs/Exercícios/ex_simples_struct.c
+++ 2019-08-21 - Structs/Exercícios/ex_simples_struct.c
@@ -2,9 +2,8 @@
 #include <string.h>
 #include <locale.h>
 
-// struct aluno agora sera chamada de Aluno
+// struct aluno agora será chamada de Aluno
 typedef struct aluno Aluno;
-
 struct aluno {
     char nome_aluno[30];
     char curso[30];
@@ -14,37 +13,37 @@ struct aluno {
 
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
-    struct aluno aluno_1;
+    Aluno aluno_1;
     Aluno aluno_2;
-    char nome[30] = "maria";
+
     // inicialização
-    Aluno aluno_3 = {nome, "letras", 0.8, 14000};
+    Aluno aluno_3 = {"Maria", "letras", 0.8, 14000};
+
     // atribuição
     aluno_1.ra = 10500;
     aluno_1.coeficiente = 0.7;
     strcpy(aluno_1.nome_aluno, "Emanoeli");
     strcpy(aluno_1.curso, "Engenharia de computação");
+
     // scanf
-    printf("Informe ra, coeficiente, nome e curso:\n");
-    scanf("%d", &aluno_2.ra);
-    __fpurge(stdin);
-    scanf("%f", &aluno_2.coeficiente);
-    __fpurge(stdin);
+    printf("Informe nome, curso, ra e coeficiente:\n");
     scanf("%[^\n]s", aluno_2.nome_aluno);
     __fpurge(stdin);
     scanf("%[^\n]s", aluno_2.curso);
-    __fpurge(stdin);
+    scanf("%d", &aluno_2.ra);
+    scanf("%f", &aluno_2.coeficiente);
 
+    printf("\n");
     printf("Nome: %s\n", aluno_1.nome_aluno);
     printf("Curso: %s\n", aluno_1.curso);
     printf("Coeficiente: %.2f\n", aluno_1.coeficiente);
     printf("RA: %d\n", aluno_1.ra);
-
+    printf("\n");
     printf("Nome: %s\n", aluno_2.nome_aluno);
     printf("Curso: %s\n", aluno_2.curso);
     printf("Coeficiente: %.2f\n", aluno_2.coeficiente);
     printf("RA: %d\n", aluno_2.ra);
-
+    printf("\n");
     printf("Nome: %s\n", aluno_3.nome_aluno);
     printf("Curso: %s\n", aluno_3.curso);
     printf("Coeficiente: %.2f\n", aluno_3.coeficiente);
