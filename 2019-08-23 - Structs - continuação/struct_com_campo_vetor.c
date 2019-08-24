d57571c13b78f0d4512bf191ce3fcd2fe46e2560
emanoelim <emanoeli.madalosso@gmail.com>
2019-08-23 21:48:22 -0300

    exercicio atualizado

--- 2019-08-23 - Structs - continuação/struct_com_campo_vetor.c
+++ 2019-08-23 - Structs - continuação/struct_com_campo_vetor.c
@@ -5,7 +5,20 @@ struct aluno {
     char nome[50];
     int ra;
     float notas[3];
+    float media;
 };
+
+void calcula_media(Aluno vetor[], int n) {
+    int i, j;
+    float media;
+    for(i = 0; i < n; i++) {
+        media = 0;
+        for(j = 0; j < 3; j++) {
+           media = media + vetor[i].notas[j];
+        }
+        vetor[i].media = media / 3;
+    }
+}
 
 int main(void) {
     Aluno vetor[3];
@@ -22,12 +35,14 @@ int main(void) {
         __fpurge(stdin);
     }
 
+    calcula_media(vetor, 3);
     for(i = 0; i < 3; i++) {
         printf("Nome: %s\n", vetor[i].nome);
         printf("RA: %d\n", vetor[i].ra);
         for(j = 0; j < 3; j++) {
             printf("Nota %d: %.2f\n", j + 1, vetor[i].notas[j]);
         }
+        printf("Media: %.2f\n", vetor[i].media);
     }
 
     return 0;
